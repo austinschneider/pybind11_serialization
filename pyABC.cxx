2c83222a3b435c10df7c6b9c755f7d9d38ae4809
Austin Schneider <hogenshpogen@gmail.com>
2023-09-27 12:54:27 -0400

    Add check for initialized self in pyABC when serializing

--- pyABC.cxx
+++ pyABC.cxx
@@ -16,7 +16,18 @@ PYBIND11_MODULE(pyABC, m) {
         .def("call", &ABC::call)
         // Register a set of pickling methods that store the python state if available
         .def(py::pickle(
-            [](const py::object &self) {
+            [](const ABC & cpp_obj) {
+                py::object self;
+                std::cout << "is pyABC instance? " << (dynamic_cast<pyABC const *>(&cpp_obj) != nullptr) << std::endl;
+                if(dynamic_cast<pyABC const *>(&cpp_obj) != nullptr and dynamic_cast<pyABC const *>(&cpp_obj)->self) {
+                    std::cout << "Grabbing self from pyABC" << std::endl;
+                    self = pybind11::reinterpret_borrow<pybind11::object>(dynamic_cast<pyABC const *>(&cpp_obj)->self);
+                } else {
+                    std::cout << "Grabbing self from global registry" << std::endl;
+                    auto *tinfo = pybind11::detail::get_type_info(typeid(ABC));
+                    pybind11::handle self_handle = get_object_handle(static_cast<const ABC *>(&cpp_obj), tinfo);
+                    self = pybind11::reinterpret_borrow<pybind11::object>(self_handle);
+                }
                 py::dict d;
                 if (py::hasattr(self, "__dict__")) {
                     d = self.attr("__dict__");
