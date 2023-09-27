f7b4cdc4124695b529c739ec5ce98504c2d330c1
Austin Schneider <hogenshpogen@gmail.com>
2023-09-27 12:14:51 -0400

    Remove direct module import. Pickle should take care of this internally

--- pyABCbindings.cxx
+++ pyABCbindings.cxx
@@ -15,7 +15,6 @@ std::string pyABC_dumper(pyABC & object) {
     } else {
         auto *tinfo = pybind11::detail::get_type_info(typeid(ABC));
         pybind11::handle self_handle = get_object_handle(static_cast<const ABC *>(&object), tinfo);
-        //pybind11::object obj = pybind11::object(self_handle, true);
         obj = pybind11::reinterpret_borrow<pybind11::object>(self_handle);
     }
     std::cout <<"#########" << std::endl;
@@ -25,13 +24,6 @@ std::string pyABC_dumper(pyABC & object) {
         std::cout << "imported pickle" << std::endl;
     else
         std::cout << "failed to import pickle" << std::endl;
-    std::cout << "importing module" << std::endl;
-    py::module mod = py::module::import("module");
-    py::object m = mod.attr("PyDerived")();
-    if(mod)
-        std::cout << "imported module" << std::endl;
-    else
-        std::cout << "failed to import module" << std::endl;
     std::cout << "dumping obj" << std::endl;
     py::bytes bytes = pkl.attr("dumps")(obj);
     std::cout << "dumped obj" << std::endl;
@@ -41,11 +33,8 @@ std::string pyABC_dumper(pyABC & object) {
 
 pyABC pyABC_loader(std::string & state) {
     pyABC object;
-    //py::bytes bytes = (py::bytes)(state);
     std::cout << "importing pickle" << std::endl;
     py::module pkl = py::module::import("pickle");
-    std::cout << "importing module" << std::endl;
-    py::module mod = py::module::import("module");
 
     std::cout << "getting fromhex" << std::endl;
     py::object fromhex = py::globals()["__builtins__"].attr("bytes").attr("fromhex");
@@ -54,8 +43,6 @@ pyABC pyABC_loader(std::string & state) {
     py::print(bytes);
 
     std::cout << "loading from bytes" << std::endl;
-    //py::object m = mod.attr("PyDerived")();
-    //mod.attr("pyABC").attr("ABC");
     pkl.attr("loads")(bytes);
     std::cout << "storing from bytes" << std::endl;
     object.self = pkl.attr("loads")(bytes);
